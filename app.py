596f4965d48a6ce690c434a75ee1e1962350576e
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-08-12 11:51:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ if menu == "Dataset":
     """, unsafe_allow_html=True)
 
     st.write('Dataset ini berisi data tentang curah hujan. Dataset yang digunakan pada penelitian ini berasal dari website https://dataonline.bmkg.go.id berdasarkan hasil pengamatan Badan Meteorologi, Klimatologi, dan Geofisika Stasiun Meteorologi Maritim Tanjung Perak dari 1 Januari 2019 hingga 31 Agustus 2023.')
-    missing_values = ['8888']
-    df = pd.read_excel('Dataset_Curah_Hujan.xlsx', na_values = missing_values)
+    df = pd.read_excel('Salinan Dataset_Curah_Hujan.xlsx')
     df['Tanggal'] = pd.to_datetime(df['Tanggal'], format='%d-%m-%Y')
     st.session_state.df = df
     st.write("Dataset Curah Hujan : ")
@@ -87,57 +86,8 @@ elif menu == "Model LSTM":
     scaler = st.session_state.scaler
     scaled_data = st.session_state.scaled_data
     if df is not None and scaler is not None and scaled_data is not None:
-        epochs = st.number_input("Masukkan nilai epoch:", min_value=1, max_value=100, value=100)
-        learning_rate = st.number_input("Masukkan nilai learning rate:", min_value=0.0001, max_value=0.01, value=0.01)
-        time_steps = st.number_input("Masukkan nilai time step:", min_value=25, max_value=75, value=25)
-        split_data = st.number_input("Masukkan nilai data train:", min_value=0.5, max_value=0.9, value=0.9)
-        st.session_state.time_steps = time_steps
-    
         if st.button('Simpan'):
-            values = scaled_data
-            training_data_len = math.ceil(len(values) * 0.7)
-            st.session_state.training_data_len = training_data_len
-            train_data = scaled_data[:training_data_len]
-
-            x_train = []
-            y_train = []
-
-            for i in range(time_steps, len(train_data)):
-                x_train.append(train_data[i - time_steps:i, 0])
-                y_train.append(train_data[i, 0])
-
-            x_train, y_train = np.array(x_train), np.array(y_train)
-            x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-            test_data = scaled_data[training_data_len - time_steps:]
-            x_test = []
-            y_test = []
-
-            for i in range(time_steps, len(test_data)):
-                x_test.append(test_data[i - time_steps:i, 0])
-                y_test.append(test_data[i, 0])
-
-            x_test, y_test = np.array(x_test), np.array(y_test)
-            x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-            st.session_state.x_train = x_train
-            st.session_state.x_test = x_test
-            st.session_state.y_train = y_train
-            st.session_state.y_test = y_test
-    
-            def build_and_train_lstm(x_train, y_train, x_test, y_test, epochs, learning_rate):
-                model = Sequential()
-                model.add(LSTM(100, name='lstm_layer_1', return_sequences=True, input_shape=(x_train.shape[1], 1)))
-                model.add(Dropout(0.5))
-                model.add(LSTM(100, name='lstm_layer_2'))
-                model.add(Dropout(0.5))
-                model.add(Dense(1))
-                optimizer = Adam(learning_rate=learning_rate)
-                model.compile(optimizer=optimizer, loss='mean_squared_error')
-                model.fit(x_train, y_train, batch_size=32, epochs=epochs, validation_data=(x_test, y_test))
-                st.session_state.model = model
-                return model
-    
-            model = build_and_train_lstm(x_train, y_train, x_test, y_test, epochs, learning_rate)
+            model = load_model('model_splitdata_0.9_epochs_100_lr_0.01_ts_25.h5')
             st.session_state.model = model
             st.write("Model telah disimpan dan dilatih.")
     else:
